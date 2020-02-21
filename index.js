c753fd58d892a1a883e3a6e24f23fa89d2223507
FDP-FromTigers18 <60490371+FDP-FromTigers18@users.noreply.github.com>
2020-02-21 13:09:07 +0800

    Add files via upload
    The MAIN ONE.

---
+++ index.js
@@ -0,0 +1,54 @@
+///INHERITANCE///
+//class Progrise{
+//    constructor(name, ability){
+//    this.name=name;
+//    this.ability=ability;
+//    }
+//
+//    create(color){
+//        console.log(`${this.name}'s color is ${color}`);
+//    }
+//
+//}
+
+//class Progrise2 extends Progrise {
+//    constructor(name, ability, ability2){
+//        super(name, ability);
+//        this.ability2 = ability2;
+//    }
+//
+//    attack(whatdoes){
+//        console.log(`${this.name}'s final attack is a ${whatdoes}.`)
+//    }
+//
+//}
+
+//let p1 = new Progrise ("Rising Hopper", "Jump");
+//let q1 = new Progrise2 ("Shooting Wolf", "Bullet")
+
+
+//p1.create("Yellow");
+//q1.attack("Rider Shoot")
+/////////////////
+
+///POLYMORPHISM///
+
+//p1 = q1;
+//p1.create("BLUE");
+
+/////////////////
+
+///ERRORHANDLING///
+function hello(){
+    
+}
+
+try{
+    hello();
+}catch(e){
+    console.log("Dividing by 0 is undefined/")
+}finally{
+    console.log("this block will always execute.")
+}
+
+//////////////////
